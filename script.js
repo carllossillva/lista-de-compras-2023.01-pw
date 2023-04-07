534ef5eea4620fd940a4f389b296b6d0e7a1fc4f
carllossillva <carlos.2020108217@unicap.br>
2023-04-06 21:55:57 -0300

    main

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
-let tabela = document.querySelector("#tabela tbody");
-let total = document.querySelector("#total");
+let products = [];
 
-function adicionarProduto() {
-  let produto = document.querySelector("#produto").value;
-  let quantidade = Number(document.querySelector("#quantidade").value);
-  let valor = Number(document.querySelector("#valor").value);
-  
+const inputItem = document.getElementById("inputItem");
+const inputQuantidade = document.getElementById("inputQuantidade");
+const inputValor = document.getElementById("inputValor");
+const BtAdicionar = document.getElementById("BtAdicionar");
+const BtLimpar = document.getElementById("BtLimpar");
+const tbody = document.getElementById("tbody");
 
-  if (produto === "" || quantidade === 0 || valor === 0) {
-    alert("Preencha todos os campos.");
-    return;
-  }
+const createListItem = (tabelaItens) => {
+tbody.innerHTML = "";
+for(let i = 0; i < tabelaItens.length; i++) {
+const tr = document.createElement("tr");
+const tdItem = document.createElement("td");
+const tdQuantidade = document.createElement("td");
+const tdValor = document.createElement("td");
+const item = document.createTextNode(tabelaItens[i].name);
+const quantidade = document.createTextNode(tabelaItens[i].quantidade);
+const valor = document.createTextNode(tabelaItens[i].valor);
 
-  let linha = document.createElement("tr");
-  let colunaProduto = document.createElement("td");
-  let colunaQuantidade = document.createElement("td");
-  let colunaValor = document.createElement("td");
-  let colunatotal = document.createElement("td");
+tdItem.appendChild(item);
+tdQuantidade.appendChild(quantidade);
+tdValor.appendChild(valor);
 
-  colunaProduto.textContent = produto;
-  colunaQuantidade.textContent = quantidade;
-  colunaValor.textContent = valor;
-  colunatotal.textContent = total;
+tr.appendChild(tdItem);
+tr.appendChild(tdQuantidade);
+tr.appendChild(tdValor);
+tbody.appendChild(tr);
 
-  linha.appendChild(colunaProduto);
-  linha.appendChild(colunaQuantidade);
-  linha.appendChild(colunaValor);
-  tabela.appendChild(linha);
-  linha.appendChild(colunatotal);
-
-  let totalAtual = Number(total.textContent);
-  let valorFinal = quantidade * valor;
-  total.textContent = totalAtual + valorFinal;
+}
 }
 
-function limparTabela() {
-  tabela.innerHTML = "";
-  total.textContent = "0";
+const hndBtnAddItem = () =>{
+const item = {
+name: inputItem.value,
+quantidade: inputQuantidade.value,
+valor: inputValor.value
+};
+
+if(!item.name || !item.quantidade || !item.valor){
+alert("Preencha todos os campos!!");
+return;
 }
+
+products.push(item);
+console.log(products);
+inputItem.value = "";
+inputQuantidade.value = "";
+inputValor.value = "";
+
+createListItem(products);
+inputItem.focus();
+}
+
+const hndBtnClearList = () =>{
+inputItem.value = "";
+inputQuantidade.value = "";
+inputValor.value = "";
+products = [];
+createListItem(products);
+inputItem.focus();
+}
+
+BtAdicionar.onclick = hnd
